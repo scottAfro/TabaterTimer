4fa0b387b493e776dbe2720f37bc433d356e3efb
scottfro <skscotkennedy@gmail.com>
2014-12-22 14:01:32 +0000

    1st Commit

---
+++ PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp1.Resources;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            //Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void btnStart_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            //Navigate to Routine page
+            NavigationService.Navigate(new Uri("/startPage.xaml", UriKind.Relative));
+        }
+      
+
+        private void btnOneDrive_Click(object sender, RoutedEventArgs e)
+        {
+            ShowInBrowser("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=12&ct=1396013362&rver=6.4.6456.0&wp=MBI_SSL_SHARED&wreply=https:%2F%2Fonedrive.live.com%2F%3Fgologin%3D1%26mkt%3Den-GB&lc=2057&id=250206&cbcxt=sky&mkt=en-GB&cbcxt=sky");
+        }
+
+        private void ShowInBrowser(string url)
+        {
+            Microsoft.Phone.Tasks.WebBrowserTask wbt = new Microsoft.Phone.Tasks.WebBrowserTask();
+            wbt.Uri = new Uri(url);
+            wbt.Show();
+            throw new NotImplementedException();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            //Navigate to Edit Routine page
+            NavigationService.Navigate(new Uri("/Edit.xaml", UriKind.Relative));
+        }
+      
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ PhoneApp1/startPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace PhoneApp1
+{
+    public partial class startPage : PhoneApplicationPage
+    {
+       Stopwatch timerWatch = new Stopwatch();
+
+       long hour, minute, second, milllisecond;
+       int i, endPrep, endRound, rounds, counter;
+
+       DispatcherTimer timer;      
+
+        public startPage()
+        {
+            InitializeComponent();
+            hour = 0;
+            minute = 0;
+            second = 0;
+            milllisecond = 0;
+            endPrep = 10;
+            endRound = 20;
+            //rounds = 8;
+            counter = 0;
+        }
+
+        private void btnBegin_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            
+            if(btnBegin.Content.ToString() == "Begin")
+            {
+                //timerWatch.Start();
+                //timer = new DispatcherTimer();
+                //timer.Interval = new TimeSpan(0, 0, 0, 1, 0);       
+                //timer.Tick += timer_Tick;
+                //timer.Start();
+                //btnPause.Visibility = System.Windows.Visibility.Visible;
+
+                //Need to start a counter for each round(8)
+                for (rounds = 0; rounds < 9; rounds++) // or if()if the timer = 10 seconds + rounds < 9                
+                {
+                    timerWatch.Start();
+                    timer = new DispatcherTimer();
+                    timer.Interval = new TimeSpan(0, 0, 0, 1, 0);                   
+                    timer.Tick += timer_Tick;
+                    timer.Start();
+                    btnPause.Visibility = System.Windows.Visibility.Visible;
+                    counter = rounds;
+                    txtblRoundNo.Text = counter.ToString();
+                }
+            }
+            else
+            {
+                 timerWatch.Stop();
+            }
+            
+        }//end of btnBegin
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            milllisecond = timerWatch.ElapsedMilliseconds;
+
+            second = milllisecond / 1000;
+            milllisecond = milllisecond % 1000;
+
+            minute = second / 60;
+            second = second % 60;
+
+            hour = minute / 60;
+            minute = minute % 60;
+
+            txtblTime.Text = minute.ToString("00") + ":" + second.ToString("00");
+           
+        }
+   
+        private void btnStop_Tap(object sender, System.Windows.Input.GestureEventArgs e)//actually the pause button
+        {
+            if (timer != null)
+            {
+                timerWatch.Stop();
+            }
+        }
+
+        private void btnPause_Tap(object sender, System.Windows.Input.GestureEventArgs e)//actually the stop button
+        {
+            //timerWatch.Stop();
+            //txtblTime.Text = minute.ToString("00") + ":" + second.ToString("00");
+            timerWatch.Restart();
+            timerWatch.Stop();
+
+        }
+    }
+}
