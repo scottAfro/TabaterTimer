54baa01235dff7eb0b238f24212169ed373ae7eb
scottfro <skscotkennedy@gmail.com>
2014-12-24 20:53:17 +0000

    EditPage modificattion

--- PhoneApp1/Edit.xaml.cs
+++ PhoneApp1/Edit.xaml.cs
@@ -12,9 +12,30 @@ namespace PhoneApp1
 {
     public partial class Edit : PhoneApplicationPage
     {
+
+        TimeSpan prepDuration = new TimeSpan(0, 0, 0, 10);
+        TimeSpan workDuration = new TimeSpan(0, 0, 0, 20);
+        TimeSpan restDuration = new TimeSpan(0, 0, 0, 10);
+
         public Edit()
         {
             InitializeComponent();
+
+            txtblEditPrepare.Text = prepDuration.ToString(@"mm\:ss");
+            txtblEditWork.Text = workDuration.ToString(@"mm\:ss");
+            txtblEditRest.Text = restDuration.ToString(@"mm\:ss"); 
+            
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void btnStartNew_Click(object sender, RoutedEventArgs e)
+        {
+            //Navigate to Edit Timer Routine page
+            NavigationService.Navigate(new Uri("/EditPageTimer.xaml", UriKind.Relative));
         }
     }
 }
