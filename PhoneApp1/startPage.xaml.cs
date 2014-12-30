85736140585d06f3fc056957ec1d34be0a1f22b1
scottfro <skscotkennedy@gmail.com>
2014-12-30 13:17:18 +0000

    ProperTimerFunction

--- PhoneApp1/startPage.xaml.cs
+++ PhoneApp1/startPage.xaml.cs
@@ -15,9 +15,12 @@ namespace PhoneApp1
     public partial class startPage : PhoneApplicationPage
     { 
        long hour, minute, second, milllisecond;
-       int i, rounds, prepTicks, workTick, restTicks;
+       int rounds, prepTicks, workTick, restTicks;
 
        DispatcherTimer timer;
+       
+
+
        Stopwatch timerWatch = new Stopwatch(); 
 
         public startPage()
@@ -28,85 +31,101 @@ namespace PhoneApp1
             second = 0;
             milllisecond = 0;            
             rounds = 1;
-            prepTicks = 05;
-            workTick = 20;
-            restTicks = 10;
+            prepTicks = 06;
+            workTick = 21;
+            restTicks = 11;
         }
 
         private void btnBegin_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            //timerWatch.Start();
+            timerWatch.Start();
+
             timer = new DispatcherTimer();
             timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
             timer.Tick += timer_Tick;
             timer.Start();
-            btnPause.Visibility = System.Windows.Visibility.Visible;        
 
-               
+           btnPause.Visibility = System.Windows.Visibility.Visible;
+
+            
+            
+                       
         }//end of btnBegin
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            ////throw new NotImplementedException();
-            //milllisecond = timerWatch.ElapsedMilliseconds;
-
-            //second = milllisecond / 1000;
-            //milllisecond = milllisecond % 1000;
-
-            //minute = second / 60;
-            //second = second % 60;
-
-            //hour = minute / 60;
-            //minute = minute % 60;
-
-            //txtblTime.Text = minute.ToString("00") + ":" + second.ToString("00");  
-            //while (rounds > 0 && rounds < 9)
-            //{
-                if (prepTicks > 0)
-                {
-                    txtblPrepare.Visibility = System.Windows.Visibility.Visible;
-                    txtblGo.Visibility = System.Windows.Visibility.Collapsed;
-                    txtblTime.Text = prepTicks + " Seconds Remaining";
-                    prepTicks--;
-                }
-                else if (workTick > 0)
-                {
-                    txtblPrepare.Visibility = System.Windows.Visibility.Collapsed;
-                    txtblGo.Visibility = System.Windows.Visibility.Visible;
-                    txtblTime.Text = workTick + " Seconds Remaining";
-                    workTick--;
-                }
-                else if (restTicks > 0)
-                {
-                    txtblGo.Visibility = System.Windows.Visibility.Collapsed;
-                    txtblRest.Visibility = System.Windows.Visibility.Visible;
-                    txtblTime.Text = restTicks + " Seconds Remaining";
-                    restTicks--;
-                }
-                else
-                {
-                    txtblRest.Text = "Congratulations";
-                    txtblTime.Text = "Times Up";
-                }
-            //    rounds++;
-                
-            //}
-            //txtblRoundNo.Text = rounds.ToString();
+            //throw new NotImplementedException();
+            if (timerWatch.Elapsed > new TimeSpan(0, 0, 0))
+            {
+                milllisecond = timerWatch.ElapsedMilliseconds;
+                second = milllisecond / 1000;
+                milllisecond = milllisecond % 1000;
+                minute = second / 60;
+                second = second % 60;
+                hour = minute / 60;
+                minute = minute % 60;
+                txtblTime.Text = minute.ToString("00") + ":" + second.ToString("00");
+            }
+            else if (timerWatch.Elapsed > new TimeSpan(0, 0, 4))
+            {
+                timerWatch.Restart();
+                milllisecond = timerWatch.ElapsedMilliseconds;
+                second = milllisecond / 1000;
+                milllisecond = milllisecond % 1000;
+                minute = second / 60;
+                second = second % 60;
+                hour = minute / 60;
+                minute = minute % 60;
+                txtblTime.Text = minute.ToString("00") + ":" + second.ToString("00"); 
+            }
+            else if (timerWatch.Elapsed > new TimeSpan(0, 0, 19) && timerWatch.Elapsed < new TimeSpan(0, 0, 10))
+            {
+                timerWatch.Restart();
+                milllisecond = timerWatch.ElapsedMilliseconds;
+                second = milllisecond / 1000;
+                milllisecond = milllisecond % 1000;
+                minute = second / 60;
+                second = second % 60;
+                hour = minute / 60;
+                minute = minute % 60;
+                txtblTime.Text = minute.ToString("00") + ":" + second.ToString("00");
+            }
+            else
+                txtblTime.Text = "Times Up!";
             
+
+                    
+               // if(prepTicks > 0)
+                // {
+                //     prepTicks--;
+                //     txtblTime.Text = prepTicks + " Seconds Remaining";
+                // }                             
+                //else if (prepTicks < 1 )
+                //{
+                //    workTick--;
+                //    txtblTime.Text = workTick + " Seconds Remaining";
+                //    txtblPrepare.Visibility = System.Windows.Visibility.Collapsed;
+                //    txtblGo.Visibility = System.Windows.Visibility.Visible;
+                //}
+                //else if (workTick < 1)
+                //{
+                //    restTicks--;
+                //    txtblTime.Text = restTicks + " Seconds Remaining";
+                //    txtblGo.Visibility = System.Windows.Visibility.Collapsed;
+                //    txtblRest.Visibility = System.Windows.Visibility.Visible;
+                //}    
         }
    
         private void btnStop_Tap(object sender, System.Windows.Input.GestureEventArgs e)//actually the pause button
         {
-            //if (prepTicks != null || workTick != null || restTicks != null)
-            //{
-            //    timerWatch.Stop();
-            //}
+           timerWatch.Stop();            
         }
 
         private void btnPause_Tap(object sender, System.Windows.Input.GestureEventArgs e)//actually the stop button
         {
             //set back to 00:00
-            
+            timerWatch.Restart();
+            timerWatch.Stop();
             txtblPrepare.Visibility = System.Windows.Visibility.Visible;
             txtblGo.Visibility = System.Windows.Visibility.Collapsed;
         }
